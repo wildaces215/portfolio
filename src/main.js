70941ed9e0588985f5dc0125422065294781cecb
wildaces215 <wannbecoastie@gmail.com>
2019-10-22 17:54:44 -0400

    Removed google analytics

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Vuesax from 'vuesax'
-import VueSwimlane from 'vue-swimlane'
-import history from 'connect-history-api-fallback'
-import 'animate.css/animate.css'
-import 'vuesax/dist/vuesax.css' // Vuesax styles
-import 'material-icons/iconfont/material-icons.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuesax from "vuesax";
+import VueSwimlane from "vue-swimlane";
+import history from "connect-history-api-fallback";
+import "animate.css/animate.css";
+import "vuesax/dist/vuesax.css"; // Vuesax styles
+import "material-icons/iconfont/material-icons.css";
+import VueAnalytics from "vue-analytics";
+import "bootstrap/dist/css/bootstrap.css";
 
-import 'bootstrap/dist/css/bootstrap.css'
-
-Vue.config.productionTip = false
-Vue.use(Vuesax)
-Vue.use(VueSwimlane)
-Vue.use(history)
+Vue.config.productionTip = false;
+Vue.use(Vuesax);
+Vue.use(VueSwimlane);
+Vue.use(VueAnalytics, {
+  id: "UA-98497114-2"
+});
+Vue.use(history);
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
