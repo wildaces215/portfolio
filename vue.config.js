ea14114c600cb85cd603c3bfcaaacf77486d783f
wildaces215 <wannbecoastie@gmail.com>
2019-12-08 19:19:20 -0500

    Hello World

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ var PrerenderSpaPlugin = require("prerender-spa-plugin");
 var path = require("path");
 module.exports = {
   lintOnSave: false,
+  //publicPath: "</../dist/index.html>",
   configureWebpack: config => {
     if (process.env.NODE_ENV === "production") {
       plugins: [
